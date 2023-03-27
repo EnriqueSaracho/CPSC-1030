33ce0e7897aaa997cb83d783399e815aa7b7b779
Enrique Saracho Felix <esarachof97@gmail.com>
2023-03-27 16:03:04 -0700

    account.html modified

--- Project/Checkers/app.js
+++ Project/Checkers/app.js
@@ -1,4 +1,8 @@
 let btn = true;
+const signupBtn = document.getElementById("signup-btn");
+const loginBtn = document.getElementById("login-btn");
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
 
 function showNavbar() {
   if (btn) {
@@ -7,4 +11,22 @@ function showNavbar() {
     document.getElementById("secondaryNav").style.top = "-91.5vh";
   }
   btn = !btn;
+}
+
+function switchForm(btn) {
+  if (btn === "signup") {
+    signupBtn.style.backgroundColor = "#ffffff";
+    signupBtn.style.boxShadow = "-5px 0px 5px -5px rgba(0, 0, 0, 0.5)";
+    loginBtn.style.backgroundColor = "#cfcfcf";
+    loginBtn.style.boxShadow = "none";
+    signupForm.style.display = "flex";
+    loginForm.style.display = "none";
+  } else {
+    loginBtn.style.backgroundColor = "#ffffff";
+    loginBtn.style.boxShadow = "5px 0px 5px -5px rgba(0, 0, 0, 0.5)";
+    signupBtn.style.backgroundColor = "#cfcfcf";
+    signupBtn.style.boxShadow = "none";
+    loginForm.style.display = "flex";
+    signupForm.style.display = "none";
+  }
 }
